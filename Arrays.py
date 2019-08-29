e0d2f2255ca9c3b7df81b863374d77c460678257
BenDevo20 <45665610+BenDevo20@users.noreply.github.com>
2019-08-29 16:12:25 -0400

    Add files via upload

---
+++ Arrays.py
@@ -0,0 +1,13 @@
+import numpy as np
+a = np.array([[0,1,2,3,4],[5,6,7,8]])
+print(a)
+print(a.shape) #outputs the shape of the array
+
+#generating random numbers
+import random
+prob = random.random() #generates random number from 0 to 1
+print(prob)
+
+number = random.randint(1,6)
+print(number) #prints random number between 1 and 6
+
